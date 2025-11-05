548f6d3e53273c3db573823662ae014f5b8e1915
kyle.vet <contact@kyle.vet>
2025-11-04 21:08:27 -0800

    Enhance VeChain.Transaction.Response with max_fee_per_gas and max_priority_fee_per_gas fields, and update cast functions for new tx type handling

--- lib/vechain/transaction/response.ex
+++ lib/vechain/transaction/response.ex
@@ -14,6 +14,8 @@ defmodule VeChain.Transaction.Response do
     :expiration,
     :gas,
     :gas_price_coef,
+    :max_fee_per_gas,
+    :max_priority_fee_per_gas,
     :id,
     :meta,
     :nonce,
@@ -30,7 +32,9 @@ defmodule VeChain.Transaction.Response do
           depends_on: Types.t_hash() | nil,
           expiration: non_neg_integer(),
           gas: non_neg_integer(),
-          gas_price_coef: non_neg_integer(),
+          gas_price_coef: non_neg_integer() | nil,
+          max_fee_per_gas: binary() | nil,
+          max_priority_fee_per_gas: binary() | nil,
           id: Types.t_hash(),
           meta: Meta.t(),
           nonce: binary(),
@@ -53,7 +57,7 @@ defmodule VeChain.Transaction.Response do
         "nonce" => nonce,
         "origin" => origin,
         "size" => tx_size,
-        "type" => tx_type
+        "type" => 0
       }) do
     %__MODULE__{
       block_ref: Utils.hex_decode!(block_ref),
@@ -73,14 +77,54 @@ defmodule VeChain.Transaction.Response do
       # origin is required (sender address)
       origin: Utils.address_to_binary!(origin),
       size: tx_size,
-      type: tx_type
+      type: 0
+    }
+  end
+
+  def cast(%{
+        "blockRef" => block_ref,
+        "chainTag" => chain_tag,
+        "clauses" => clauses,
+        "delegator" => delegator,
+        "dependsOn" => depends_on,
+        "expiration" => expiration,
+        "gas" => gas,
+        "maxFeePerGas" => max_fee_per_gas,
+        "maxPriorityFeePerGas" => max_priority_fee_per_gas,
+        "id" => id,
+        "meta" => meta,
+        "nonce" => nonce,
+        "origin" => origin,
+        "size" => tx_size,
+        "type" => 81
+      }) do
+    %__MODULE__{
+      block_ref: Utils.hex_decode!(block_ref),
+      chain_tag: chain_tag,
+      clauses: Clause.cast_all(clauses),
+      # delegator is nullable (used in fee delegation)
+      delegator: Utils.maybe_address_to_binary(delegator),
+      # depends_on is nullable (used for transaction dependencies)
+      depends_on: Utils.maybe_hex_decode(depends_on),
+      expiration: expiration,
+      gas: gas,
+      max_fee_per_gas: Utils.hex_decode!(max_fee_per_gas),
+      max_priority_fee_per_gas: Utils.hex_decode!(max_priority_fee_per_gas),
+      # id is required (transaction hash)
+      id: Utils.hex_decode!(id),
+      meta: Meta.cast(meta),
+      nonce: Utils.hex_decode!(nonce),
+      # origin is required (sender address)
+      origin: Utils.address_to_binary!(origin),
+      size: tx_size,
+      type: 81
     }
   end
 
   defimpl Inspect do
     alias VeChain.Utils
 
-    def inspect(tx_response, opts) do
+    def inspect(%{type: 0} = tx_response, opts) do
       # Use the default struct inspection with the modified tx_response
       %{
         tx_response
@@ -88,6 +132,23 @@ defmodule VeChain.Transaction.Response do
           delegator: Utils.hex_encode(tx_response.delegator),
           depends_on: Utils.hex_encode(tx_response.depends_on),
           id: Utils.hex_encode(tx_response.id),
+          origin: Utils.hex_encode(tx_response.origin),
+          nonce: Utils.hex_encode(tx_response.nonce),
+          type: Utils.type_to_atom(tx_response.type)
+      }
+      |> Inspect.Any.inspect(opts)
+    end
+
+    def inspect(%{type: 81} = tx_response, opts) do
+      # Use the default struct inspection with the modified tx_response
+      %{
+        tx_response
+        | block_ref: Utils.hex_encode(tx_response.block_ref),
+          delegator: Utils.hex_encode(tx_response.delegator),
+          depends_on: Utils.hex_encode(tx_response.depends_on),
+          id: Utils.hex_encode(tx_response.id),
+          max_fee_per_gas: Utils.hex_encode(tx_response.max_fee_per_gas),
+          max_priority_fee_per_gas: Utils.hex_encode(tx_response.max_priority_fee_per_gas),
           origin: Utils.hex_encode(tx_response.origin),
           nonce: Utils.hex_encode(tx_response.nonce),
           type: Utils.type_to_atom(tx_response.type)
