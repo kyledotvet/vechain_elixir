ea628bc2c87bdce57dd0f0ef35f98d7e79a9e53e
kyle.vet <contact@kyle.vet>
2026-02-13 11:19:38 -0800

    Refactor Legacy transaction module: enhance type definitions, improve documentation, and streamline struct attributes for better clarity and usability

--- lib/vechain/transaction/legacy.ex
+++ lib/vechain/transaction/legacy.ex
@@ -1,7 +1,15 @@
 defmodule VeChain.Transaction.Legacy do
+  @moduledoc """
+  Module representing Legacy transactions on the VeChain blockchain.
+
+  This struct and its associated functions handle the specific fields and encoding logic for Legacy transactions, which include the `gas_price_coef` field. The module also implements the necessary protocols for RLP encoding and inspection.
+
+  Most users will interact with this module indirectly through `VeChain.Transaction`, which will determine the appropriate behavior based on the struct and configuration.
+  """
   alias VeChain.Transaction.Reserved
   alias VeChain.Transaction.Clause
   alias VeChain.Transaction.Signature
+  alias VeChain.Transaction
   alias VeChain.Utils
 
   defstruct [
@@ -20,22 +28,29 @@ defmodule VeChain.Transaction.Legacy do
     reserved: %Reserved{}
   ]
 
+  @type gas_price_coef() :: binary()
+
   @type t() :: %__MODULE__{
-          id: binary() | nil,
-          chain_tag: binary(),
-          block_ref: binary(),
-          expiration: binary(),
-          gas_price_coef: binary(),
-          gas: binary(),
-          depends_on: binary() | nil,
-          nonce: binary(),
-          signature: binary() | nil,
-          origin: binary() | nil,
-          delegator: binary() | nil,
-          clauses: [Clause.t()],
+          id: Transaction.id(),
+          chain_tag: Transaction.chain_tag(),
+          block_ref: Transaction.block_ref(),
+          expiration: Transaction.expiration(),
+          gas_price_coef: gas_price_coef(),
+          gas: Transaction.gas(),
+          depends_on: Transaction.depends_on(),
+          nonce: Transaction.nonce(),
+          signature: Transaction.signature(),
+          origin: Transaction.origin(),
+          delegator: Transaction.delegator(),
+          clauses: Transaction.clauses(),
           reserved: Reserved.t()
         }
 
+  @doc """
+  Creates a new Legacy transaction struct based on passed in attributes . The expectation is that the attributes are coming from `VeChain.Transaction.new/1` and have already been validated and transformed into the correct format (binary). Odds are good that if you are calling this function directly, you may be doing something wrong.
+
+  Raises if unsupported fields are provided.
+  """
   def new(%{max_fee_per_gas: _anything}),
     do: raise(ArgumentError, "Legacy transactions do not use max_fee_per_gas")
 
@@ -43,10 +58,15 @@ defmodule VeChain.Transaction.Legacy do
     do: raise(ArgumentError, "Legacy transactions do not use max_priority_fee_per_gas")
 
   def new(attrs) do
-    # TODO: Migrate this to validate attrs and convert to binary
     struct(__MODULE__, attrs)
   end
 
+  @doc """
+  Casts a RLP decoded list into a Legacy transaction struct.
+  This is used internally when decoding raw transactions from the blockchain.
+
+  Ref: `VeChain.Transaction.typed_cast/1`
+  """
   def cast([
         chain_tag,
         block_ref,
@@ -73,6 +93,10 @@ defmodule VeChain.Transaction.Legacy do
     |> Signature.maybe_parse()
   end
 
+  @doc """
+  Converts a Legacy transaction struct into a list format suitable for RLP encoding. This is used internally when encoding transactions to be sent to the blockchain.
+  """
+  @spec to_rlp_list(t()) :: list()
   def to_rlp_list(tx) do
     # Everything is "stored" in the struct as binary so we can just
     # return the fields in the correct order for RLP encoding
@@ -107,7 +131,6 @@ defmodule VeChain.Transaction.Legacy do
     alias VeChain.Utils
 
     def inspect(transaction, opts) do
-      # Use the default struct inspection with the prepared fields
       %{
         transaction
         | id: Utils.hex_encode(transaction.id),
